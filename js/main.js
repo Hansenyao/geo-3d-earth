0e67c34e17fa6c3f4ee1ec8b75f46943e306c7f1
hansenyao <yyfsingler@163.com>
2026-04-15 09:26:21 -0600

    create render function

--- js/main.js
+++ js/main.js
@@ -11,3 +11,29 @@ camera.position.z = 3;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// 4. Create geometry
+const geometry = new THREE.SphereGeometry(1, 64, 64);
+
+// 5. Texture
+const texture = new THREE.TextureLoader().load(
+    'https://threejs.org/examples/textures/earth_atmos_2048.jpg'
+);
+
+// 6. meterial (shader)
+const material = new THREE.MeshBasciMaterial({map: texture});
+
+// 7. mesh (geometry + material)
+const earth = new THREE.Mesh(geometry, material);
+scene.add(earth);
+
+// Render loop
+function render() {
+    requestAnimationFrame(render);
+
+    earth.rotation.y += 0.001;
+
+    renderer.render(scene, camera);
+}
+
+render();
