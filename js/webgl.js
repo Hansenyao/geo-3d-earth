d85b995b641cbf95e3f097f136fce1639442c6e6
Hansenyao <yyfsingler@163.com>
2026-04-16 03:17:30 -0600

    Add GLSL to create shaders for WebGL

--- js/webgl.js
+++ js/webgl.js
@@ -12,3 +12,37 @@ const gl = canvas.getContext("webgl");
 if (!gl) {
   alert("WebGL not supported");
 }
+
+// ===========================
+// Use GLSL to create shaders
+// ===========================
+
+// Vertex shader, render each vertex for WebGL
+const vsSource = `
+attribute vec3 aPosition;
+uniform mat4 uMatrix;
+
+void main() {
+  gl_Position = uMatrix * vec4(aPosition, 1.0);
+}
+`;
+
+// Fragment shader, render color for each pixel
+const fsSource = `
+precision mediump float;
+
+void main() {
+  gl_FragColor = vec4(0.2, 0.8, 1.0, 1.0);
+}
+`;
+
+// Compile shaders
+function createShader(gl, type, source) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  return shader;
+}
+
+const vs = createShader(gl, gl.VERTEX_SHADER, vsSource);
+const fs = createShader(gl, gl.FRAGMENT_SHADER, fsSource);
